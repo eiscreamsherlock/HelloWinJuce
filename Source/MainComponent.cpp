566c30d5357f42a10c09938834bae17ac882b952
David S <shuler.david.p+fork@gmail.com>
2025-03-04 23:49:10 +0100

    Resolved those graphics chnages

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,9 +16,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::FontOptions (32.0f));
-    g.setColour (juce::Colours::aquamarine);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setFont (juce::FontOptions (48.0f));
+    g.setColour (juce::Colours::magenta);
+    g.drawText ("Graphics are Updated!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
