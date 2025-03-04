5f0f59f0c6dfdbcd35663ef36ff970ec6b59f596
David S <shuler.david.p+fork@gmail.com>
2025-03-04 23:26:17 +0100

    Update font size and color

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,8 +16,8 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::FontOptions (16.0f));
-    g.setColour (juce::Colours::white);
+    g.setFont (juce::FontOptions (32.0f));
+    g.setColour (juce::Colours::aquamarine);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
